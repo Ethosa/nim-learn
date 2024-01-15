a0f8cda5519e1f03abeef23e081d2a42fe824b5e
Ethosa <social.ethosa@gmail.com>
2024-01-15 23:12:04 +0700

    2 series

---
+++ lesson_2/main.nim
@@ -0,0 +1,15 @@
+var a = 10  # runtime
+var b: int = 1230
+var c: int
+
+a = 123  # done
+
+let x: int = 10  # runtime
+let y = 1
+
+# x = 11  # failure
+
+const i = 10  # compile-time
+# i = 13123  # failure
+
+echo x + y  # 11
